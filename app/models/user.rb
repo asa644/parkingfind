050c18541c3ce6cef1daaf7ba8204b92435af948
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2016-08-16 15:26:08 +0300

    put application in a sort of functioning state

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
 
   has_many :bookings
-  has_many :parking_spots, through: :bookings
+  has_many :parking_spots
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.to_h.slice(:provider, :uid)
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,16 @@
 Rails.application.routes.draw do
 
-  # get 'booking/index'
-
-  # get 'booking/new'
-
-  # get 'booking/create'
-
-  # get 'booking/show'
-
-  # get 'booking/edit'
-
-  # get 'booking/update'
-
-  # get 'booking/destroy'
+  root to: 'pages#home'
 
   resources :parking_spots do
     resources :bookings, only: [:new, :create, :destroy]
   end
+
   get '/profile' => 'users#show', as: :profile
 
-
+  resources :users, only: [:show] do
+    resources :parking_spots, only: [:show]
+  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
