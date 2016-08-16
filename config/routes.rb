d2bc0721eb36b4b152847d91eb0182279b3a8c1b
Athmane Hannae <athmane.world@gmail.com>
2016-08-16 18:41:54 +0300

    routes fix the error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
 
   resources :parking_spots do
     resources :bookings, only: [:new, :create, :destroy]
