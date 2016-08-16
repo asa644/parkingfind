4aac30ef89f1f92a18e2aa8ad9a4b40774d73e39
Athmane Hannae <athmane.world@gmail.com>
2016-08-16 11:09:55 +0300

    Adding Facebook Connect

---
+++ app/controllers/parking_spots_controller.rb
@@ -0,0 +1,23 @@
+class ParkingSpotsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @parking = ParkingSpot.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  get 'parking_spots/index'
+
+  get 'parking_spots/show'
+
+  get 'parking_spots/new'
+
+  get 'parking_spots/create'
+
+  get 'parking_spots/edit'
+
+  get 'parking_spots/update'
+
+  get 'parking_spots/destroy'
+
   get '/profile/:id' => 'users#show', as: :profile
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
