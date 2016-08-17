0a5dbf2a3491031b7d9dcb3fc2d3b35573b21aab
Benjamin Gabay <bengabay@gmail.com>
2016-08-17 15:01:44 +0300

    added picture feature to parking_spots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816093559) do
+ActiveRecord::Schema.define(version: 20160817114735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160816093559) do
     t.text     "description"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "photo"
     t.index ["user_id"], name: "index_parking_spots_on_user_id", using: :btree
   end
 
