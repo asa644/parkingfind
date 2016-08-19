0112e6d8d416883b89b41505b74850a48136cb50
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2016-08-19 12:58:17 +0300

    test

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(version: 20160818151304) do
     t.index ["user_id"], name: "index_parking_spots_on_user_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
