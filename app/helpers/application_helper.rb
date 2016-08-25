28a0f79e43b2341b12046c19c531931254aa3920
Benjamin Gabay <bengabay@gmail.com>
2016-08-25 14:51:22 +0300

    login modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,22 @@ module ApplicationHelper
     image_tag user.avatar,
               opts
   end
+
+# modal for login !
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    User
+  end
+
 end
